6c360497bbc8310eb7bcdd6fbb638f47165605bb
Ratul <ratulbamangachi@gmail.com>
2023-01-16 01:00:11 +0530

    increased password and user cookie set time

--- admin/index.php
+++ admin/index.php
@@ -14,8 +14,8 @@ if(isset($_POST['submit'])){
     $data=mysqli_fetch_assoc(mysqli_query($con,"SELECT * FROM `login` WHERE user_name='$username' and user_pass='$password'"));
     if($data){
         if(isset($_POST['check'])){
-            setcookie('username',$username,time()+30);
-            setcookie('password',$password,time()+30);
+            setcookie('username',$username,time()+60*60*60);
+            setcookie('password',$password,time()+60*60*60);
         }else{
             setcookie('username',$username,30);
             setcookie('password',$password,30);
