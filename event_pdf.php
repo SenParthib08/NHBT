912e2d28dbb500b2dabded8bd2b925a4a56b8802
SenJeet <jeetcrj08@gmail.com>
2023-01-16 03:23:51 +0530

    Event page added

---
+++ event_pdf.php
@@ -0,0 +1,207 @@
+<?php
+include('config/config.php');
+include('track.php');
+include('header.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Our Events</title>
+    <link rel="stylesheet" href="css/event_pdf.css" />
+    <link rel="stylesheet" href="css/header.css" />
+  </head>
+  <body>
+    <div class="event-container">
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-date">
+            <div class="date">16</div>
+            <div class="month">Jan</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-title">Event 1</h3>
+
+          <div class="event-description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+          <br>
+          <div class="event-description-large">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit facilis distinctio quis quaerat? Ex amet dignissimos magnam saepe porro harum impedit libero, recusandae aperiam adipisci odit. Omnis necessitatibus porro eligendi.
+          </div>
+
+          <div class="event-timing">
+            <!-- <img src="images/time.png" alt="" /> 10:00 am -->
+            <button class="btn"> <a href="#">View Details</a> </button>
+            <button class="btn"> <a href="#">Poster template</a> </button>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="event">
+        <div class="event-left">
+          <div class="event-date">
+            <div class="date">17</div>
+            <div class="month">Jan</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-title">Event 2</h3>
+
+          <div class="event-description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+          <br>
+          <div class="event-description-large">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit facilis distinctio quis quaerat? Ex amet dignissimos magnam saepe porro harum impedit libero, recusandae aperiam adipisci odit. Omnis necessitatibus porro eligendi.
+          </div>
+
+          <div class="event-timing">
+            <!-- <img src="images/time.png" alt="" /> 10:00 am -->
+            <button class="btn"> <a href="#">View Details</a> </button>
+            <button class="btn"> <a href="#">Poster template</a> </button>
+          </div>
+        </div>
+      </div>
+
+
+     
+     
+      <div class="event">
+        <div class="event-left">
+          <div class="event-date">
+            <div class="date">18</div>
+            <div class="month">Jan</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-title">Event 3</h3>
+
+          <div class="event-description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+          <br>
+          <div class="event-description-large">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit facilis distinctio quis quaerat? Ex amet dignissimos magnam saepe porro harum impedit libero, recusandae aperiam adipisci odit. Omnis necessitatibus porro eligendi.
+          </div>
+
+          <div class="event-timing">
+            <!-- <img src="images/time.png" alt="" /> 10:00 am -->
+            <button class="btn"> <a href="#">View Details</a> </button>
+            <button class="btn"> <a href="#">Poster template</a> </button>
+          </div>
+        </div>
+      </div>
+
+      
+      
+      <div class="event">
+        <div class="event-left">
+          <div class="event-date">
+            <div class="date">19</div>
+            <div class="month">Jan</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-title">Event 4</h3>
+
+          <div class="event-description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+          <br>
+          <div class="event-description-large">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit facilis distinctio quis quaerat? Ex amet dignissimos magnam saepe porro harum impedit libero, recusandae aperiam adipisci odit. Omnis necessitatibus porro eligendi.
+          </div>
+
+          <div class="event-timing">
+            <!-- <img src="images/time.png" alt="" /> 10:00 am -->
+            <button class="btn"> <a href="#">View Details</a> </button>
+            <button class="btn"> <a href="#">Poster template</a> </button>
+          </div>
+        </div>
+      </div>
+
+      
+      
+      <div class="event">
+        <div class="event-left">
+          <div class="event-date">
+            <div class="date">20</div>
+            <div class="month">Jan</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-title">Event 5</h3>
+
+          <div class="event-description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+          <br>
+          <div class="event-description-large">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit facilis distinctio quis quaerat? Ex amet dignissimos magnam saepe porro harum impedit libero, recusandae aperiam adipisci odit. Omnis necessitatibus porro eligendi.
+          </div>
+
+          <div class="event-timing">
+            <!-- <img src="images/time.png" alt="" /> 10:00 am -->
+            <button class="btn"> <a href="#">View Details</a> </button>
+            <button class="btn"> <a href="#">Poster template</a> </button>
+          </div>
+        </div>
+      </div>
+
+      
+      
+      <div class="event">
+        <div class="event-left">
+          <div class="event-date">
+            <div class="date">21</div>
+            <div class="month">Jan</div>
+          </div>
+        </div>
+
+        <div class="event-right">
+          <h3 class="event-title">Event 6</h3>
+
+          <div class="event-description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione.
+          </div>
+          <br>
+          <div class="event-description-large">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus,
+            ratione. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit facilis distinctio quis quaerat? Ex amet dignissimos magnam saepe porro harum impedit libero, recusandae aperiam adipisci odit. Omnis necessitatibus porro eligendi.
+          </div>
+
+          <div class="event-timing">
+            <!-- <img src="images/time.png" alt="" /> 10:00 am -->
+            <button class="btn"> <a href="#">View Details</a> </button>
+            <button class="btn"> <a href="#">Poster template</a> </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+<?php
+include('footer.php');
+?>
